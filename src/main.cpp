1811fc1c4d0e9eca9bb8c02bbb7a4047fd6a5669
Constantinus <motoff.konstantin@yandex.ru>
2025-08-19 19:12:36 +0300

    Изменение структуры проекта и рефакторинг кода - Обновлен CMakeLists.txt - Убрана инициализация библиотеки mpg123, а именно mpg123_init() и mpg123_exit() (в новых версиях библиотеки это не нужно делать) - Добавлены и изменены некоторые комментарии - Измены описание ошибок - Функции завершения библиотеки mpg123 и освобождения памяти были перенесены в отдельную функцию CleanUp - Убран тестовый вывод всех доступных аудиоустройств - Добавлены свои переменные PaError для каждой из проверок на ошибки - Заголовочные файлы внешних библиотек были вынесены в отдельные подпапки

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,74 @@
+#include <chrono>
+#include <cstring>
 #include <iostream>
+#include <thread>
+
+// внешние библиотеки
 #include <mpg123.h>
 #include <portaudio.h>
-#include <cstring>
-#include <thread>
-#include <chrono>
 
 template <typename T>
-void FreePtr(T *&ptr)
+void FreeBuffer(T *&ptr)
 {
     if (ptr)
     {
-        delete[] ptr;
+        delete ptr;
         ptr = nullptr;
     }
 }
 
+// Освобождение handle
+void CleanUp(mpg123_handle *&mh, unsigned char *&buffer)
+{
+    if (mh)
+    {
+        mpg123_close(mh);
+        mpg123_delete(mh);
+        mh = nullptr;
+    }
+
+    FreeBuffer(buffer);
+}
+
 int main()
 {
-    // Инициализация mpg123
     mpg123_handle *mh = nullptr;
     unsigned char *buffer = nullptr;
     size_t buffer_size = 0;
-    int err = 0;
-    int channels = 0, encoding = 0;
+    int channels = 0;
+    int encoding = 0;
     long rate = 0;
+    int err = MPG123_OK;
 
-    // Инициализация библиотеки mpg123
-    if (mpg123_init() != MPG123_OK)
+    // Создание дескриптора. Указываем NULL для выбора дескриптора по умолчанию
+    mh = mpg123_new(NULL, &err);
+    if (mh == nullptr)
     {
-        std::cerr << "Failed to initialize mpg123 library" << std::endl;
-        return 1;
-    }
-
-    // Создание дескриптора декодера
-    mh = mpg123_new(nullptr, &err);
-    if (!mh)
-    {
-        std::cerr << "Failed to create mpg123 handle: " << mpg123_plain_strerror(err) << std::endl;
-        mpg123_exit();
+        std::cerr << "Failed to create mpg123_handle. Error: " << mpg123_plain_strerror(err) << std::endl;
         return 1;
     }
 
     // Открытие MP3-файла, пока просто тестирование, можно заменить на любой mp3 файл
     if (mpg123_open(mh, "C:/file.mp3") != MPG123_OK)
     {
-        std::cerr << "Failed to open MP3 file: " << mpg123_strerror(mh) << std::endl;
-        mpg123_delete(mh);
-        mpg123_exit();
+        std::cerr << "Failed to open mp3 file. Error: " << mpg123_strerror(mh) << std::endl;
         return 1;
     }
 
-    // Получение информации о формате аудио
+    // Получение информации о формате
     if (mpg123_getformat(mh, &rate, &channels, &encoding) != MPG123_OK)
     {
-        std::cerr << "Failed to get audio format" << std::endl;
-        mpg123_close(mh);
-        mpg123_delete(mh);
-        mpg123_exit();
+        std::cerr << "Failed to get audio format. Error: " << mpg123_strerror(mh) << std::endl;
+        CleanUp(mh, buffer);
         return 1;
     }
 
-    
+    // Устанавливается стандартный формат 16-битный PCM
     encoding = MPG123_ENC_SIGNED_16;
     if (mpg123_format(mh, rate, channels, encoding) != MPG123_OK)
     {
-        std::cerr << "Failed to set output format" << std::endl;
-        mpg123_close(mh);
-        mpg123_delete(mh);
-        mpg123_exit();
+        std::cerr << "Failed to set output format. Error: " << mpg123_strerror(mh) << std::endl;
+        CleanUp(mh, buffer);
         return 1;
     }
 
@@ -77,131 +78,116 @@ int main()
     buffer = new unsigned char[buffer_size];
 
     // Инициализация PortAudio
-    PaError paErr = Pa_Initialize();
-    if (paErr != paNoError)
+    const PaError pa_init_err = Pa_Initialize();
+    if (pa_init_err != paNoError)
     {
-        std::cerr << "PortAudio initialization failed: " << Pa_GetErrorText(paErr) << std::endl;
-        FreePtr(buffer);
-        mpg123_close(mh);
-        mpg123_delete(mh);
-        mpg123_exit();
+        std::cerr << "PortAudio initialization failed. Error: " << Pa_GetErrorText(pa_init_err) << std::endl;
+        CleanUp(mh, buffer);
         return 1;
     }
 
-    // Поиск устройства VB-Cable
-    int vbCableIndex = -1;
-    int numDevices = Pa_GetDeviceCount();
-    const char *targetDeviceName = "CABLE Input";
+    // Поиск необходимого устройства Input Cabel (VB-Cabel)
+    int vb_cabel_index = -1;
+    const int num_devices = Pa_GetDeviceCount();
+    const char *target_device = "CABLE Input";
 
-    for (int i = 0; i < numDevices; i++)
+    for (int i = 0; i < num_devices; ++i)
     {
-        const PaDeviceInfo *deviceInfo = Pa_GetDeviceInfo(i);
-        if (deviceInfo->maxOutputChannels > 0)
+        const PaDeviceInfo *device_info = Pa_GetDeviceInfo(i);
+        if (strstr(device_info->name, target_device) != nullptr)
         {
-            std::cout << "Found audio device #" << i << ": " << deviceInfo->name << std::endl;
-            if (strstr(deviceInfo->name, targetDeviceName) != nullptr)
-            {
-                vbCableIndex = i;
-                break;
-            }
+            vb_cabel_index = i;
+            break;
         }
     }
 
-    if (vbCableIndex == -1)
+    if (vb_cabel_index == -1)
     {
-        std::cerr << "VB-Cable output device not found!" << std::endl;
+        std::cerr << "CABLE Input device not found!" << std::endl;
         Pa_Terminate();
-        FreePtr(buffer);
-        mpg123_close(mh);
-        mpg123_delete(mh);
-        mpg123_exit();
+        CleanUp(mh, buffer);
         return 1;
     }
 
-    std::cout << "Using VB-Cable device at index: " << vbCableIndex << std::endl;
+    PaStreamParameters output_parameters =
+        {
+            .device = vb_cabel_index,
+            .channelCount = channels,
+            .sampleFormat = paInt16,
+            .suggestedLatency = Pa_GetDeviceInfo(vb_cabel_index)->defaultLowOutputLatency,
+            .hostApiSpecificStreamInfo = nullptr};
 
-    // Настройка параметров вывода
-    PaStreamParameters outputParameters;
-    outputParameters.device = vbCableIndex;
-    outputParameters.channelCount = channels;
-    outputParameters.sampleFormat = paInt16;
-    outputParameters.suggestedLatency = Pa_GetDeviceInfo(vbCableIndex)->defaultLowOutputLatency;
-    outputParameters.hostApiSpecificStreamInfo = nullptr;
+    const PaError pa_format_err = Pa_IsFormatSupported(nullptr, &output_parameters, rate);
+    if (pa_format_err != paNoError)
+    {
+        std::cerr << "The format is not supported. Error: " << Pa_GetErrorText(pa_format_err) << std::endl;
+        Pa_Terminate();
+        CleanUp(mh, buffer);
+        return 1;
+    }
 
     PaStream *stream = nullptr;
-    paErr = Pa_OpenStream(
+    const PaError pa_open_err = Pa_OpenStream(
         &stream,
-        nullptr,                      // Нет входного потока
-        &outputParameters,            // Параметры вывода
-        rate,                         // Частота дискретизации
-        paFramesPerBufferUnspecified, // Автоматический размер буфера
-        paClipOff,                    // Отключаем клиппинг
-        nullptr,                      // Нет callback-функции
-        nullptr                       // Нет пользовательских данных
-    );
+        nullptr,
+        &output_parameters,
+        rate,
+        paFramesPerBufferUnspecified,
+        paClipOff,
+        nullptr,
+        nullptr);
 
-    if (paErr != paNoError)
+    if (pa_open_err != paNoError)
     {
-        std::cerr << "Failed to open audio stream: " << Pa_GetErrorText(paErr) << std::endl;
+        std::cerr << "Failed to open audio stream. Error: " << Pa_GetErrorText(pa_open_err) << std::endl;
         Pa_Terminate();
-        FreePtr(buffer);
-        mpg123_close(mh);
-        mpg123_delete(mh);
-        mpg123_exit();
+        CleanUp(mh, buffer);
         return 1;
     }
 
     using namespace std::chrono_literals;
     std::this_thread::sleep_for(5s);
-	
-    // Запуск аудиопотока
-    paErr = Pa_StartStream(stream);
-    if (paErr != paNoError)
+
+    const PaError pa_start_err = Pa_StartStream(stream);
+    if (pa_start_err != paNoError)
     {
-        std::cerr << "Failed to start audio stream: " << Pa_GetErrorText(paErr) << std::endl;
+        std::cerr << "Failed to start audio stream. Error: " << Pa_GetErrorText(pa_start_err) << std::endl;
         Pa_CloseStream(stream);
         Pa_Terminate();
-        FreePtr(buffer);
-        mpg123_close(mh);
-        mpg123_delete(mh);
-        mpg123_exit();
-        return 1;
+        CleanUp(mh, buffer);
     }
 
-    std::cout << "Streaming audio to VB-Cable..." << std::endl;
+    std::cout << "Streaming audio..." << std::endl;
 
     // Основной цикл воспроизведения
     size_t done = 0;
+    PaError pa_write_err;
     while ((err = mpg123_read(mh, buffer, buffer_size, &done)) == MPG123_OK)
     {
+        // Если mpg123 прочитал 0 байт
         if (done == 0)
         {
             std::this_thread::sleep_for(std::chrono::milliseconds(10));
             continue;
         }
 
-        paErr = Pa_WriteStream(stream, buffer, done / (channels * sizeof(short)));
-        if (paErr != paNoError)
+        pa_write_err = Pa_WriteStream(stream, buffer, done / (channels * sizeof(short)));
+        if (pa_write_err != paNoError)
         {
-            std::cerr << "Audio write error: " << Pa_GetErrorText(paErr) << std::endl;
+            std::cerr << "Failed to write audio. Error: " << Pa_GetErrorText(pa_write_err) << std::endl;
             break;
         }
     }
 
     if (err != MPG123_DONE)
     {
-        std::cerr << "MP3 decoding error: " << mpg123_strerror(mh) << std::endl;
+        std::cerr << "MP3 decoding error. Error: " << mpg123_strerror(mh) << std::endl;
     }
 
-    // Завершение воспроизведения
     std::cout << "Playback finished" << std::endl;
     Pa_StopStream(stream);
     Pa_CloseStream(stream);
     Pa_Terminate();
-    FreePtr(buffer);
-    mpg123_close(mh);
-    mpg123_delete(mh);
-    mpg123_exit();
-
+    CleanUp(mh, buffer);
     return 0;
 }
