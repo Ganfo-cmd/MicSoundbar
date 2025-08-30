b834e9850f62cf507a068c74318848e2cb040dda
Constantinus <motoff.konstantin@yandex.ru>
2025-08-30 12:09:36 +0300

    Удаление файлов для тестирования интеграции GUI

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,6 @@
 #include <portaudio.h>
 #include <QApplication>
 
-#include "mainwindow.h"
-
 template <typename T>
 void FreeBuffer(T *&ptr)
 {
@@ -35,13 +33,6 @@ void CleanUp(mpg123_handle *&mh, unsigned char *&buffer)
 
 int main(int argc, char *argv[])
 {
-    // Тестирвование
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
-    // Тестирвование
-
     mpg123_handle *mh = nullptr;
     unsigned char *buffer = nullptr;
     size_t buffer_size = 0;
